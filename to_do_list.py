12af2c56a3f12182ad4cdf9bb87c914c8a15b41c
CarsonVail99 <carsonvail99@gmail.com>
2025-08-11 18:12:21 -0600

    Implemented user friendly ui 2nd Window added Saving Feature added

--- to_do_list.py
+++ to_do_list.py
@@ -1,74 +1,132 @@
-def menu(task_list=None):
-    if task_list is None:
-        task_list = []
-    print('WHAT WOULD YOU LIKE TO DO?')
-    print('1.ADD TASK')
-    print('2.SHOW ALL TASKS')
-    print('3.DELETE TASKS')
-    print('4.EXIT')
-    choice = input('ENTER YOUR CHOICE (1-4): ')
-    if choice == '1':
-        return add_task(task_list)
-    elif choice == '2':
-        return show_all_tasks(task_list)
-    elif choice == '3':
-        return delete_task_func(task_list)
-    elif choice == '4':
-        print('THANK YOU FOR BEING PRODUCTIVE')
-        return None
-    else:
-        print('INVALID CHOICE')
-        return menu()
+import sys
+from PyQt5.QtWidgets import QCheckBox, QMainWindow, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit
+from PyQt5.QtGui import QIcon
+from datetime import datetime
 
-def add_task(task_list):
-    while True:
-        task = input('ENTER TASK')
-        task_list.append(task)
-        add_another_task = input('DO YOU WANT TO ADD ANOTHER TASK? Y/N')
-        if add_another_task == 'Y':
-            print('TASKS ADDED')
-            continue
-        elif add_another_task.upper() == 'N':
-            return menu(task_list)
-        else:
-            print('INVALID CHOICE')
-            return menu(task_list)
+data_time=[]
+jobs = []
+class Second_Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(100,100,640,480)
+        self.setStyleSheet("background-color:grey;")
+        self.setWindowTitle("To Do List")
+        self.icon = QIcon("who is me.png")
+        self.setWindowIcon(self.icon)
+        self.initUI()
+
+    def initUI(self):
+        self.checkbox()
+        self.header()
+        self.go_back_button()
+
+    def checkbox(self):
+        self.important_widget = QLabel('Talk to Alt\nIts always speaking to you')
+        self.important_widget.setGeometry(10, 10, 630, 100)
+        self.important_widget.setStyleSheet('font-size: 20px;')
+        self.central_widget = QWidget()
+        self.central_widget.setGeometry(10, 10, 630, 400)
+        self.setCentralWidget(self.central_widget)
+        self.vbox = QVBoxLayout(self.central_widget)
+        self.vbox.addWidget(self.important_widget)
+        for job in jobs:
+            self.show_checkbox = QCheckBox(f'{job}', self)
+            self.show_checkbox.setStyleSheet('font-size: 20px;')
+            self.vbox.addWidget(self.show_checkbox)
+
+    def header(self):
+        Title = QLabel("Tasks", self)
+        Title.setGeometry(10, 10, 630, 40)
+        Title.setStyleSheet('font-size: 30px;')
+
+    def go_back_button(self):
+        self.back = QPushButton('Back', self)
+        self.back.setGeometry(400, 420, 100, 50)
+        self.back.clicked.connect(self.go_back_func)
+
+    def go_back_func(self):
+        self.First_Window = First_Window()
+        self.First_Window.show()
+        self.hide()
 
 
-def show_all_tasks(task_list, return_to_menu=True):
-    print('YOUR TASKS ARE:')
-    if not task_list:
-        print('NO TASKS')
-    else:
-        for index, task in enumerate(task_list, start=1):
-            print(f'{index}. {task}')
-    if return_to_menu:
-        return menu(task_list)
+class First_Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(100,100,640,480)
+        self.initUI()
+        self.Second_Window = None
 
-def delete_task_func(task_list):
-    if not task_list:
-        print('NO TASKS')
-        return menu(task_list)
+    def initUI(self):
+        self.setWindowTitle("To Do List")
+        self.icon = QIcon("who is me.png")
+        self.setWindowIcon(self.icon)
+        self.setStyleSheet("background-color:grey;")
+        self.linedit()
+        self.next_button()
+        self.add_task_button()
+        self.close_button()
+    def linedit(self):
+        self.line = QLineEdit(self)
+        self.line.setPlaceholderText('Enter Task Here...')
+        self.line.setGeometry(10, 10, 540, 40)
+        self.line.setStyleSheet('font-size: 20px;')
 
-    show_all_tasks(task_list, return_to_menu=False)
-    try:
-        delete = int(input('ENTER THE TASK YOU WANT TO DELETE:'))
-        if 1<= delete <= len(task_list):
-            delete_task = task_list.pop(delete-1)
-            print(f'TASK {delete_task} DELETED')
-        else:
-            print('INVALID CHOICE')
-    except ValueError:
-        print('INVALID CHOICE')
-    return menu(task_list)
+    def add_task_button(self):
+        self.add_task = QPushButton('Add Task', self)
+        self.add_task.setGeometry(10, 100, 630, 55)
+        try:
+            self.add_task.clicked.connect(self.handle_add_task)
+        except Exception:
+            pass
+
+    def handle_add_task(self):
+        jobs.append(self.line.text())
+        self.line.clear()
+
+    def next_button(self):
+        self.next = QPushButton('show tasks', self)
+        self.next.setGeometry(5, 420, 630, 50)
+        self.next.clicked.connect(self.open_second_window)
+
+
+    def open_second_window(self):
+        self.Second_Window = Second_Window()
+        self.Second_Window.show()
+        self.hide()
+
+    def close_button(self):
+        self.close_button = QPushButton('Close/Save', self)
+        self.close_button.setGeometry(5, 360, 630, 50)
+        self.close_button.clicked.connect(self.save_data)
+
+    def save_data(self):
+        data_extractor()
+        self.close()
+
+
+
+def data_extractor():
+    now = datetime.now()
+    current_time = now.strftime('%y-%m-%d %H:%M:%S') + 100*'*'+ ('Alt is always speaking to you')
+    with open('important_data.txt', 'a') as f:
+        f.write(f'{100*"*"}\n{current_time}\n{jobs}\n{100*"*"}{'Listen'}')
 
 
 
 def main():
-    task_list = []
-    menu(task_list)
+    app = QApplication(sys.argv)
+    window = First_Window()
+    window.show()
+    sys.exit(app.exec_())
 
 
 
 if __name__ == '__main__':
     main()
+
+
+
+
+

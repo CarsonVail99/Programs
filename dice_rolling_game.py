6b2ac8fe0f2289ff5a431a82832405d1b10ceaa4
CarsonVail99 <carsonvail99@gmail.com>
2025-08-31 18:40:14 -0600

    Complete GUI implemented with added functionality of new game and exit buttons

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -2,84 +2,139 @@ import random
 import sys
 
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget
-                            ,QLabel, QVBoxLayout, QPushButton)
+                            ,QLabel, QVBoxLayout, QPushButton, QDesktopWidget)
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QIcon, QPixmap
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.player_score = 0
         self.computer_score = 0
+        self.winning_score = 3
+        self.center()
         self.initUI()
+        self.show()
+
+    def center(self):
+        #Get geometry of the screen
+        screen = QDesktopWidget().screenGeometry()
+
+        #Get the geometry of the window
+        size = self.geometry()
+
+        #Center the window
+        self.move(int((screen.width() - size.width()) / 2),
+                  int((screen.height() - size.height()) / 2))
+
 
     def initUI(self):
+        #Create Window
         self.setWindowTitle('Dice Rolling Game')
-        self.setGeometry(1400, 800, 300, 300)
+        self.setGeometry(0, 0, 400, 400)
 
-        #create a central widget and main layout
+        #Create a central widget and main layout
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
-        layout = QVBoxLayout(central_widget)
+        self.layout = QVBoxLayout(central_widget)
 
-        #create score display
-        self.score_label = QLabel(f'Player score: 0 Computer score: 0')
-        self.score_label.setAlignment(Qt.AlignCenter)
-        self.score_label.setFont(QFont('Arial', 18))
+        #Window Icon
+        self.setWindowIcon(QIcon('dice.jpeg'))
 
-        #create roll results display
-        rolldice_widget = QPushButton('Roll Dice', self)
-        rolldice_widget.clicked.connect(self.player_roll)
-        dice_rolled = QLabel('')
-        exit_widget = QPushButton('Exit',self)
+        #Create score display
+        self.score_label = QLabel(f'Player score: {self.player_score} Computer score: {self.computer_score}')
+        self.score_label.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
+        self.score_label.setFont(QFont('Arial', 25))
+
+        #Declare Winner
+        self.winner_label = QLabel('')
+        self.winner_label.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
+        self.winner_label.setFont(QFont('Arial', 25))
+
+        #Create roll results display
+        self.dice_rolled = QLabel('')
+        self.dice_rolled.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
+        self.dice_rolled.setFont(QFont('Arial', 25))
+
+        #Roll dice Widget
+        self.rolldice_widget = QPushButton('Roll Dice')
+        self.rolldice_widget.clicked.connect(self.roll)
+        self.rolldice_widget.setFont(QFont('Arial', 25))
+
+        #Exit Widget
+        exit_widget = QPushButton('Exit')
         exit_widget.clicked.connect(self.close)
+        exit_widget.setFont(QFont('Arial', 25))
+
+        #New Game Widget
+        self.new_game_widget = QPushButton('Play Again')
+        self.new_game_widget.clicked.connect(self.play_again)
+        self.new_game_widget.setFont(QFont('Arial', 25))
+        self.new_game_widget.hide()
 
         #Add widgets to a layout
-        layout.addWidget(dice_rolled)
-        layout.addWidget(self.score_label)
-        layout.addWidget(rolldice_widget)
-        layout.addWidget(exit_widget)
+        self.layout.addWidget(self.dice_rolled)
+        self.layout.addWidget(self.score_label)
+        self.layout.addWidget(self.rolldice_widget)
+        self.layout.addWidget(exit_widget)
+        self.layout.addWidget(self.new_game_widget)
+        self.layout.addWidget(self.winner_label)
 
-    @staticmethod
+
     def computer_roll(self):
         return random.randint(1,12)
 
-    @staticmethod
     def player_roll(self):
-        self.computer_roll(self)
         return random.randint(1,12)
 
-    def game(self):
-        player_score = 0
-        computer_score = 0
-        while player_score < 3 and computer_score < 3:
-            comp_roll = self.computer_roll(self)
-            play_roll = self.player_roll(self)
-            if self.computer_roll(self) > self.player_roll(self):
-                computer_score += 1
-                self.score_label.setText(f'Player score: {player_score} Computer score: {computer_score})
+    def roll(self):
+        while True:
+            if self.player_score == self.winning_score:
+                self.winner_label.setStyleSheet('color: green')
+                self.winner_label.setText('You Won!')
+                self.rolldice_widget.setEnabled(False)
+                self.new_game_widget.show()
 
-            elif play_roll > comp_roll:
-                player_score += 1
-                print(f'player rolled: {play_roll}, computer rolled: {comp_roll} \n'
-                f'Player won!\nPlayer score is:', player_score, f'Computer score is:', computer_score)
-            elif play_roll == comp_roll:
-                print(f'Both you and computer rolled same number, Reroll')
+            elif self.computer_score == self.winning_score:
+                self.winner_label.setText('Computer Won!')
+                self.winner_label.setStyleSheet('color: red')
+                self.rolldice_widget.setEnabled(False)
+                self.new_game_widget.show()
+
+
+            player_roll = self.player_roll()
+            computer_roll = self.computer_roll()
+            self.dice_rolled.setText(f'Player rolled: {player_roll} Computer rolled:{computer_roll}')
+            if player_roll > computer_roll:
+                self.player_score += 1
+                self.score_label.setText(f'Player Score: {self.player_score} Computer Score: {self.computer_score}')
+                return self.player_score
+            elif computer_roll > player_roll:
+                self.computer_score += 1
+                self.score_label.setText(f'Player Score: {self.player_score} Computer Score: {self.computer_score}')
+                return self.computer_score
+            elif player_roll == computer_roll:
+                self.score_label.setText(f'Player Score: {self.player_score} Computer Score: {self.computer_score}')
+                return None
             else:
-                print("Invalid input")
-        if player_score == 3:
-            print(f'Player won!\nPlayer score is:', player_score, f'Computer score is:', computer_score)
-            return False
-        else:
-            print(f'Computer won!\nPlayer score is:', player_score, f'Computer score is:', computer_score)
-            return False
+                continue
 
-    # @staticmethod
+    def play_again(self):
+        self.player_score = 0
+        self.computer_score = 0
+        self.winner_label.setText('')
+        self.dice_rolled.setText('')
+        self.score_label.setText(f'Player Score: {self.player_score} Computer Score: {self.computer_score}')
+        self.rolldice_widget.setEnabled(True)
+
+
+
+
 def main():
     app = QApplication(sys.argv)
     window = Window()
     window.show()
-    app.exec_()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()

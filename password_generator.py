1065e800aa5a9f8ece278cd9c248e9a08966f9fa
CarsonVail99 <carsonvail99@gmail.com>
2025-09-05 22:09:40 -0600

    Fully implemented GUI

--- password_generator.py
+++ password_generator.py
@@ -1,34 +1,91 @@
 import random
 import string
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import Qt
+import sys
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Password Generator')
+        self.setGeometry(100, 100, 640, 480)
+        self.enter_button = None
+        self.length = None
+        self.status = None
+        self.line_edit = None
+
+        self.initUI()
+        self.center()
 
 
-def length_generate():
-    try:
-        length = int(input('Enter password length:'))
-        if length < 8:
-            print('Password length should be greater than 8')
-            return length_generate()
-        elif length > 16:
-            print('Password length should be less than 16')
-            return length_generate()
-        else:
-            return length
-    except ValueError as e:
-        print(e)
+    def center(self):
+        screen = QApplication.desktop().screenGeometry()
+        size = self.geometry()
+        x = int((screen.width() - size.width()) / 2)
+        y = int((screen.height() - size.height()) / 2)
+        self.move(x, y)
 
 
-def pw_generate(length):
-    letters = string.ascii_letters
-    digits = string.digits
-    special_char = string.punctuation
-    all_char = letters + digits + special_char
-    password = ''.join(random.choice(all_char) for i in range(length))
-    print(f'Your password is: {password}')
+    def initUI(self):
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        self.status = QLabel('')
+
+        self.enter_button = QPushButton('Enter')
+        self.enter_button.clicked.connect(self.button_clicked)
+
+        self.line_edit = QLineEdit()
+        self.line_edit.setPlaceholderText('Enter Password Length between 8-16')
+
+        icon = QIcon('C:/Users/Admin/PycharmProjects/Programs/password_icon.png')
+        self.setWindowIcon(icon)
+
+        image = QPixmap('C:/Users/Admin/PycharmProjects/Programs/password_icon.png')
+
+        image_label = QLabel()
+        image_label.setPixmap(image)
+
+        layout = QVBoxLayout()
+
+        central_widget.setLayout(layout)
+
+        layout.addWidget(self.status)
+
+        layout.addWidget(image_label)
+
+        layout.addWidget(self.line_edit)
+
+        layout.addWidget(self.enter_button)
+
+
+    def button_clicked(self):
+        self.length = self.line_edit.text()
+        if self.length.isdigit():
+            self.length = int(self.length)
+            if self.length >= 8 and self.length <= 16:
+                self.pw_generate()
+            else:
+                self.status.setText('That integer is not within the range 8-16')
+
+
+    def pw_generate(self):
+        letters = string.ascii_letters
+        digits = string.digits
+        special_char = string.punctuation
+        all_char = letters + digits + special_char
+        password = ''.join(random.choice(all_char) for i in range(self.length))
+        self.line_edit.setText(password)
+        self.status.setStyleSheet('color: green')
+        self.status.setText('Password Generated')
 
 
 def main():
-    pw_generate(length_generate())
-
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
